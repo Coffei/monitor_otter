d30bad8fb12427303783d5170ab3c2ff0661691d
Jonas Trantina <jonas.trantina@altworx.com>
2020-03-21 18:31:57 +0100

    Properly handle empty dates in Job entity

--- lib/monitor_otter_web/views/root_view.ex
+++ lib/monitor_otter_web/views/root_view.ex
@@ -1,11 +1,13 @@
 defmodule MonitorOtterWeb.RootView do
   use MonitorOtterWeb, :view
 
-  def format_ts(time) do
+  def format_ts(%DateTime{} = time, _default) do
     time
     |> Timex.Timezone.convert("Europe/Prague")
     |> Timex.format!("{D}.{M}.{YYYY} {h24}:{m}")
   end
+
+  def format_ts(_, default), do: default
 
   def hostname(url) do
     regex = ~r/^.+:\/\/([^\/?#]+)/
